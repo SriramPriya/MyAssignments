82edc53cab4569b306bf1ba4cc1374de2434f1a6
SriramPriya <priyavelraja.91@gmail.com>
2024-06-24 23:55:35 +0530

    Add files via upload
    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,33 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//** Version declaration - Data type :float **
+		float version = 91.0f;
+		
+		//** Developer declaration - Data type :String **
+		String developer = "Google";
+		
+		//** Beta Version declaration - Data type :boolean **
+		boolean isBeta = false;
+		
+		//** Release Year declaration - Data type :int**
+		int releaseYear = 2008;
+		
+		//** Shortcut Key declaration - Data type :char **
+		char shortcutKey = 'C';
+		
+		//** This will printout the declared variables**
+		System.out.println("Chrome Details are listed out below");
+		System.out.println();
+		System.out.println("Version : " +version);
+		System.out.println("Developer : " +developer);
+		System.out.println("Beta : " +isBeta);
+		System.out.println("Release Year : " +releaseYear);
+		System.out.println("Shortcut Key : " +shortcutKey);
+			}
+
+}
---
+++ day1/FireFox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FireFox {
+	public static void main(String[] args)
+	{
+	
+	float browserVersion = 100.2f;
+	String browserName = "firefox";
+	boolean isVisible = true;
+	int releaseYear  = 1998;
+	char browserLogo = 'f';
+	System.out.println("The browser Version is "+browserVersion);
+	System.out.println("The browser Name is "+browserName);
+	System.out.println("The browser is visible "+isVisible);
+	System.out.println("The browser release Year is "+releaseYear);
+	System.out.println("The browser Logo is "+browserLogo);
+}
+}
